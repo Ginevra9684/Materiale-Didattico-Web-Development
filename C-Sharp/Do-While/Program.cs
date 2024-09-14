764dccce12b970e9a34eefe52a07a196137ce3bd
Ginevra9684 <ginevravallebona98@gmail.com>
2024-09-14 10:32:41 +0200

    Do While

---
+++ C-Sharp/Do-While/Program.cs
@@ -0,0 +1,10 @@
+﻿// do-while uguale a while ma se le condizioni non sono soddisfatte compila comunque almeno una volta ciò che si trova al suo interno
+
+int numero = 10;
+
+do
+{
+    Console.WriteLine(numero);
+    numero--;
+}
+while (numero > 0);
