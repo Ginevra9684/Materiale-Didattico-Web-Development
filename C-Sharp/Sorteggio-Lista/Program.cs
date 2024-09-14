398b84232ac062055516788f07d0bb054f2c4a78
Ginevra9684 <ginevravallebona98@gmail.com>
2024-09-14 11:38:35 +0200

    Sorteggio Lista

---
+++ C-Sharp/Sorteggio-Lista/Program.cs
@@ -0,0 +1,30 @@
+﻿List<int> numeri = new List<int>();
+        numeri.Add(10);
+        numeri.Add(20);
+        numeri.Add(30);
+
+Random random = new Random();
+int indice = random.Next(0,3);
+
+Console.WriteLine("Indovina il numero sorteggiato");
+
+while(true)
+{
+    int numero = int.Parse(Console.ReadLine()!);
+
+    Console.Clear();
+s
+    if (numero == numeri[indice])
+    {
+    Console.WriteLine("Hai indovinato");
+    break;
+    }
+    else if (numero < numeri[indice])
+    {
+    Console.WriteLine("Prova un numero più grande");
+    }
+    else
+    {
+    Console.WriteLine("Prova un numero più picolo");
+    }
+}
