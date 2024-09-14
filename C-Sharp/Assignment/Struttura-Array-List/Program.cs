bcd6e4ba59e0c87abeb7308c1b0a9d12e94c5d56
Ginevra9684 <ginevravallebona98@gmail.com>
2024-09-14 11:04:39 +0200

    Assignment Array List

---
+++ C-Sharp/Assignment/Struttura-Array-List/Program.cs
@@ -0,0 +1,133 @@
+﻿/*
+other option
+                 case 3:      //!!!need to change the project and use bool values to make it work!!!
+                addedNames.Sort();
+                Console.WriteLine("d-Descending");
+                rearrange = Console.ReadKey(true).KeyChar;
+                if (rearrange == "d") addedNames.Reverse();
+                break;
+*/
+
+// Creation of an empty list, value "name" and "choice"
+
+List<string> addedNames = new List<string> ();
+string name;
+int choice;
+string rearrange;
+string search;
+
+            // The app lets us picking up options until we choose to exit
+
+do
+{           
+    Console.WriteLine("Choose an option between the following:");
+
+    Console.WriteLine("1. Add name");
+    Console.WriteLine("2. See added names");
+    Console.WriteLine("3. Rearrange your list");
+    Console.WriteLine("4. Search a name of your choice inside the list");
+    Console.WriteLine("5. Remove name");
+    Console.WriteLine("6. Overwrite a name");
+    Console.WriteLine("7. Exit");
+
+    choice = int.Parse(Console.ReadLine()!);
+
+    switch (choice) 
+    {
+        case 1:         // To add a name to our list
+            Console.Write("Insert here the name you want to add\t");
+            name = Console.ReadLine()!.Trim();
+
+            if(addedNames.Contains(name))
+            {
+                Console.WriteLine($"Impossibility to add : {name} is already in the list!!");
+            }
+            else
+            {
+                addedNames.Add(name);
+            }
+            break;                                         
+        case 2:         // To see our list
+            Console.WriteLine($"The current participants are:{addedNames.Count}\n The complete list is:");
+
+            foreach (string added in addedNames)
+            {
+            Console.WriteLine(added);
+            }
+            break;
+        case 3:         // To rearrange our list
+            Console.WriteLine("Do you prefer rearrange the list in alphabetical or reversed order, or both (A/R/B) ?");
+
+            rearrange = Console.ReadLine()!.Trim();
+
+            if (rearrange == "A" )
+            {
+                addedNames.Sort();
+            }
+            else if (rearrange == "R")
+            {
+                addedNames.Reverse();
+            }
+            else if (rearrange == "B")
+            {
+                addedNames.Sort();
+                addedNames.Reverse();
+            }
+            else
+            {
+                Console.WriteLine("The current option is not valid");
+            }
+            break;
+        case 4:
+            Console.WriteLine("What name do you want to search?");
+
+            search = Console.ReadLine()!;
+            if (addedNames.Contains(search))
+            {
+                Console.WriteLine($"{search} is in the list");
+            }
+            else
+            {
+                Console.WriteLine($"{search} is not in the list");
+            }
+            break;
+        case 5:
+            Console.WriteLine("What name do you want to remove?");
+            
+            name = Console.ReadLine()!.Trim();
+
+            if(addedNames.Contains(name))
+            {
+                addedNames.Remove(name);
+                Console.WriteLine($"{name}'s name has been succesfully removed!");
+            }
+            else
+            {
+                Console.WriteLine($"{name}'s name doesn't appear in this list, therefore it can't be removed");
+            }
+            break;
+        case 6:
+            Console.WriteLine("Which name do you want to overwrite");
+            name = Console.ReadLine()!;
+            if (addedNames.Contains(name))
+            {
+                Console.Write("Enter the new name:\t");
+                string newName = Console.ReadLine()!;
+                int index = addedNames.IndexOf(name);
+                addedNames[index] = newName;
+                Console.WriteLine("The edit occured successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"{name} doesn't appear in this list");
+            }
+            break;
+        case 7:         // To stop the cicle
+            Console.WriteLine("This session will be closed :)");
+            break;
+        default:
+        Console.WriteLine("The current option is not valid");
+        break;
+    }
+}
+while (choice != 7);
