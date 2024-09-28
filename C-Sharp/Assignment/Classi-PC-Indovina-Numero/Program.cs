f1c7c81e531871e28c0320b14142009722eecf62
Ginevra9684 <ginevravallebona98@gmail.com>
2024-09-28 10:49:07 +0200

    Assignment Classi PC Indovina Numero

---
+++ C-Sharp/Assignment/Classi-PC-Indovina-Numero/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string choice;
+        int chances;
+        int max;
+
+        Console.WriteLine("Welcome to the Number Guessing game");
+        Console.WriteLine("Basic game gives 5 chances to guess a number between 0 and 50");
+        Console.WriteLine("Do you want to set your own parameters ? (y/n)");
+        choice = Console.ReadLine()!;
+        switch(choice)
+        {
+            case "y":
+                Console.Write("Chances : ");
+                chances = Validator.CheckNumber();
+                Console.Write("Max number : ");
+                max = Validator.CheckNumber();
+                Game game = new Game(chances, max);
+                game.Play();
+                break;
+            case "n":
+                Game gameStandard = new Game();
+                gameStandard.Play();
+                break;
+        }
+    }
+}
